365d682603dfcfc31746d15e83aa06473bb91bfa
Pedro <pedrinhofrota22@gmail.com>
2025-10-27 14:29:44 -0300

    Primeiros arquivos no repositorio

---
+++ HTML/EXERCICIOS/ATV01/script.js
@@ -0,0 +1,8 @@
+const botao = document.getElementById('btn-alerta');
+
+botao.addEventListener('click', function() {
+  alert('QUEM CLICAR É BAITOLA!');
+
+botao.textContent = "Clica aqui mano";  
+});
+    
